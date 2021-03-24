eafd772c0dede7acb393a25c1c034d2766ed8bf6
tnordquist <toddnordquist144@gmail.com>
2021-03-23 18:47:20 -0600

    Fixed issues.

--- app/src/main/java/edu/cnm/deepdive/gallery12/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/gallery12/controller/MainActivity.java
@@ -5,12 +5,8 @@ import android.os.Bundle;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.View;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import edu.cnm.deepdive.gallery12.R;
